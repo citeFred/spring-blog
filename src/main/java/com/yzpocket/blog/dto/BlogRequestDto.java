ac6776772d8089d57e9b7846372ff5dedca67618
INYONG KIM <ohnyong87@gmail.com>
2023-09-05 19:17:17 +0900

    test : Blog CRUD test, POSTMAN API test.
    * Remove Spring-Security when the project's not include Auth config.
    - DI Spring-Security on build.gradle, auth will be must required
    - POSTMAN test will be failed on up situation

--- src/main/java/com/yzpocket/blog/dto/BlogRequestDto.java
+++ src/main/java/com/yzpocket/blog/dto/BlogRequestDto.java
@@ -2,15 +2,10 @@ package com.yzpocket.blog.dto;
 
 import lombok.Getter;
 
-import java.time.LocalDateTime;
-
 @Getter
 public class BlogRequestDto {
-    private Long id;
     private String title;
     private String name;
     private String contents;
-    private LocalDateTime createAt;
-    private LocalDateTime modifiedAt;
     private String password;
 }
--- src/test/java/com/yzpocket/blog/TransactionTest.java
+++ src/test/java/com/yzpocket/blog/TransactionTest.java
@@ -24,24 +24,29 @@ public class TransactionTest {
     @Test
     @Transactional
     @Rollback(value = false) // 테스트 코드에서 @Transactional 를 사용하면 테스트가 완료된 후 롤백하기 때문에 false 옵션 추가
-    @DisplayName("메모 생성 성공")
+    @DisplayName("글 생성 성공")
     void test1() {
         Blog blog = new Blog();
         blog.setName("Robbert");
         blog.setContents("@Transactional 테스트 중!");
+        blog.setTitle("Title");
+        blog.setPassword("pass");
 
-        em.persist(blog);  // 영속성 컨텍스트에 메모 Entity 객체를 저장합니다.
+        em.persist(blog);  // 영속성 컨텍스트에 글 Entity 객체를 저장합니다.
     }
 
     @Test
-    @Disabled //더이상 테스트안하니까 이것 미사용
-    @DisplayName("메모 생성 실패")
+    //@Transactional //트랜잭셔널 환경이 아니면 entity가 persistence (MANAGED)상태가 될 수 없어서 에러, 이것 주석 해제하면 작동잘됨
+    @Disabled //테스트 할 때 주석 (현재 미사용)
+    @DisplayName("글 생성 실패")
     void test2() {
         Blog blog = new Blog();
-        blog.setName("Robbie");
+        blog.setName("Robbert");
         blog.setContents("@Transactional 테스트 중!");
+        blog.setTitle("Title");
+        blog.setPassword("pass");
 
-        em.persist(blog);  // 영속성 컨텍스트에 메모 Entity 객체를 저장합니다.
+        em.persist(blog);  // 영속성 컨텍스트에 글 Entity 객체를 저장합니다.
     }
 
     @Test
