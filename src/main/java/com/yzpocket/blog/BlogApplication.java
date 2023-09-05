fc2cda88f94361d4f2c15b338ccaa439c5b62232
INYONG KIM <ohnyong87@gmail.com>
2023-09-05 23:42:09 +0900

    fix : Disable auto security configuration

--- src/main/java/com/yzpocket/blog/BlogApplication.java
+++ src/main/java/com/yzpocket/blog/BlogApplication.java
@@ -2,10 +2,11 @@ package com.yzpocket.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class BlogApplication {
 
     public static void main(String[] args) {
